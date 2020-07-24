a7cf459b28a51839a2187c40b96dde61c5cdd776
Anisha Kapoor <anisha1594@gmail.com>
2020-07-25 00:28:30 +0530

    Project Draft

---
+++ src/main/java/com/centime/tasks/fullnameservice/FullnameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.centime.tasks.fullnameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullnameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullnameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centime/tasks/fullnameservice/FullnameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centime.tasks.fullnameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullnameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
